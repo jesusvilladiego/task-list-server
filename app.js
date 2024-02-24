c3e296a04f5705680548e0b14bf15075f10ecc80
jesus villadiego <jesusvilladiegoarteaga@gmail.com>
2024-02-24 11:17:28 -0500

    cree la autorizacion

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.port
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const KEY = process.env.SECRET_KEY
 
 const listadetareas = require("./data")
-const listviewrouter = require("./src/list-view-router")
-const listeditrouter = require("./src/list-edit-router")
+const listviewrouter = require("./list-view-router")
+const listeditrouter = require("./list-edit-router")
 
 app.use("/list-view-router", listviewrouter);
 app.use("/list-edit-router", listeditrouter);
+app.use(express.json());
 
+const users = [
+  { email: 'jesus@gmail.com', password: '123458' },
+  { email: 'david@gmail.com', password: '95353235' }
+];
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((user) => user.email == email && user.password == password);
+
+  if (user) {
+    const token = jwt.sign(user, KEY);
+    res.status(200).json({ token });
+  }else{
+    res.status(401).json({ message: 'correo o contraseña incorrecta' });
+  }
+  
+
+  const token = jwt.sign({ userId: user.id, username: user.username }, process.env.SECRET_KEY, { algorithm: 'HS256' });
+  res.json({ token });
+});
 
 
 app.get("/this-should-exists", (req, res)=>{
@@ -29,4 +54,4 @@ app.use('/vertareas', listviewrouter, () => {
   console.log("estas viendo la lista de tareas")
 })
 
-app.listen(port, () => {console.log(`Servidor Express funcionando en http://localhost:${port}`);});
+app.listen(PORT, () => {console.log(`Servidor Express funcionando en http://localhost:${PORT}`);});
