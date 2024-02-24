6da32dd968f461e3e3fb017842e545599f30814d
jesus villadiego <jesusvilladiegoarteaga@gmail.com>
2024-02-24 12:23:27 -0500

    cree la autorizacion

--- app.js
+++ app.js
@@ -15,13 +15,12 @@ app.use("/list-edit-router", listeditrouter);
 app.use(express.json());
 
 const users = [
-  { email: 'jesus@gmail.com', password: '123458' },
-  { email: 'david@gmail.com', password: '95353235' }
+  { email: 'jesus@gmail.com', password: '123458' , rol: "admin" },
+  { email: 'david@gmail.com', password: '95353235', rol: "usuario"}
 ];
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-
   const user = users.find((user) => user.email == email && user.password == password);
 
   if (user) {
@@ -31,11 +30,26 @@ app.post('/login', (req, res) => {
     res.status(401).json({ message: 'correo o contraseña incorrecta' });
   }
   
-
   const token = jwt.sign({ userId: user.id, username: user.username }, process.env.SECRET_KEY, { algorithm: 'HS256' });
   res.json({ token });
 });
 
+app.get('/protegida', (req, res) => {
+  const token = req.headers.authorization;
+
+
+  jwt.verify(token, KEY, (error, decoded) => {
+    if (error) {
+      return res.status(401).json({ error });
+    }
+
+    if (decoded.rol == "admin") {
+      return res.status(200).json({users})
+    } else {
+      return res.status(4001).json({message: "No tienes acceso a esta ruta"})
+    }
+  });
+});
 
 app.get("/this-should-exists", (req, res)=>{
   res.status(404).send("Not found")
