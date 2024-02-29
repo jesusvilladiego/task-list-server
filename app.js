3f21ed21878e93e29d6c9ef24bae7516f9725fae
jesus villadiego <jesusvilladiegoarteaga@gmail.com>
2024-02-29 00:34:24 -0500

    lo puse a funcionar el servidor

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const KEY = process.env.SECRET_KEY
-
+const dotenv = require("dotenv")
+const app = express();
+const PORT = 3000
 const listadetareas = require("./data.js")
 const listviewrouter = require("./list-view-router")
 const listeditrouter = require("./list-edit-router")
+const KEY = process.env.SECRET_KEY
+dotenv.config();
 
 app.use(express.json());
 
@@ -53,11 +53,11 @@ app.get('/', (req, res) => {
   res.json(listadetareas);
 });
 
-app.use('/editar', listeditrouter, () =>{
+app.use('/editartareas', listeditrouter, () =>{
   console.log("estas editando la lista de tareas")
 });
 
-app.use('/ver', listviewrouter, () => {
+app.use('/vertareas', listviewrouter, () => {
   res.json(listadetareas)
   console.log("estas viendo la lista de tareas")
 })
